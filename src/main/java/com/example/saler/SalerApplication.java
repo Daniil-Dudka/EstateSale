a10eb64464fd828c6a57449af407158f5eb3fc2c
Daniil-Dudka <dudinskiy.d@mail.ru>
2023-03-29 23:29:24 +0300

    create a project add some dependencies

---
+++ src/main/java/com/example/saler/SalerApplication.java
@@ -0,0 +1,13 @@
+package com.example.saler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/saler/SalerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
