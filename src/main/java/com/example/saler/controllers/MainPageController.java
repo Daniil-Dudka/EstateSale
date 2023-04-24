8243f9145d8c2a1b1835b87fd6131651f6740fe9
Daniil-Dudka <dudinskiy.d@mail.ru>
2023-04-24 20:00:43 +0300

    add dependencies for db

---
+++ src/main/java/com/example/saler/controllers/MainPageController.java
@@ -0,0 +1,18 @@
+package com.example.saler.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/main")
+public class MainPageController{
+    @GetMapping
+    public String list(){
+        return "index";
+    }
+}
